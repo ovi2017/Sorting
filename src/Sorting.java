8927fd63a646a1580a47c0e95bb37340b807591a
Ovidio Castillo <oc210@email.vccs.edu>
2017-12-10 09:23:46 -0500

    Initial commit

---
+++ src/Sorting.java
@@ -0,0 +1,17 @@
+//Author: Ovidio Castillo
+//Date: 12/05/2017
+//purpose: TO sort random numbers and put them in order
+//License: GNU
+
+
+
+public class Sorting {
+
+    public static void main(String [] args){
+
+        String [] list = {"10","5","9","8","7","6","2","3","1","4"};
+        for (int i=0; i<list.length; i++) {
+            System.out.println(list[i]);
+        }
+    }
+}
